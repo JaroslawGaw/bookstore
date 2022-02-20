b27831cfb9fa89d39a94d6c7f7c32e4a40ad6451
Jarosław Gawęda <jaroslaw.gaw@gmail.com>
2022-02-20 13:36:00 +0100

    correcting errors

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -6,18 +6,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/")
-    public String index() {
-        return "index";
-    }
-    @RequestMapping("/myAccount")
-    public String myAccount() {
-        return "myAccount";
-    }
-
-    @RequestMapping("/login")
-    public String login(Model model){
-        model.addAttribute("classActiveLogin", true);
-        return "myAccount";
-    }
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("classActiveLogin", true);
+		return "myAccount";
+	}
+	
+	@RequestMapping("/forgetPassword")
+	public String forgetPassword(Model model) {
+		model.addAttribute("classActiveForgetPassword", true);
+		return "myAccount";
+	}
+	
+	@RequestMapping("/newUser")
+	public String newUser(Model model) {
+		model.addAttribute("classActiveNewUser", true);
+		return "myAccount";
+	}
 }
